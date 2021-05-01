3e4d38a3cb79fa470c78d26cda792eed7b76fca7
KernelA <17554646+KernelA@users.noreply.github.com>
2021-05-01 18:10:42 +0300

    README added

--- ml_project/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(name="ml_project",
-      version="0.0.1",
-      author="KernelA",
-      author_email="None",
-      license="MIT",
-      python_requires=">=3.8",
-      )
