f1819bb9a509ebc30f2614b76a9624b8333c1634
KernelA <17554646+KernelA@users.noreply.github.com>
2021-05-10 01:08:24 +0300

    Docker support added

--- online_inference/setup.py
+++ online_inference/setup.py
@@ -1,9 +1,17 @@
 from setuptools import setup
+from setuptools import find_packages
 
-setup(name="homework2",
+with open("requirements.txt", "r", encoding="utf-8") as req_file:
+    reqs = list(map(str.strip, req_file.readlines()))
+
+setup(name="hear_diss_app",
+      description="Simple web app on FastAPI.",
       version="0.0.1",
-      author="None",
-      author_email= "None",
+      url="https://github.com/made-ml-in-prod-2021/kernela",
+      author="KernelA",
+      author_email="None",
       license="MIT",
-      python_requires=">=3.8",
+      packages=find_packages(),
+      python_requires=">=3.7",
+      install_requires=reqs
       )
